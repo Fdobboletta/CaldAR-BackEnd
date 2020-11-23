0556bbb92be37da9367b0f1a76e99d7e13b01662
samueltrillo <knubbe10@gmail.com>
2020-11-23 17:18:34 -0300

    GetCustomerBy - type,email,discal_adress added; Delete by Id added

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -19,3 +19,56 @@ app.get("/getCustomerById/:id", (req,res) =>{
         res.status(400).json({ msg: `No customer with the id of ${req.params.id}`})
     }
 });
+
+//getCustomerByCustomerType
+app.get("/getCustomerByCustomerType/:customerType", (req,res) => {
+    const found = customers.some(customer => customer.customerType === String(req.params.customerType));
+    if (found) {
+        res.json(customers.filter(customer => customer.customerType === String(req.params.customerType)));
+    } else {
+        res.status(400).json({ msg: `No customer with the customer type ${req.params.customerType}`});
+    }
+});
+
+//getCustomerByEmail
+app.get("/getCustomerByEmail/:email", (req,res) => {
+    const found = customers.some(customer => customer.email === String(req.params.email));
+    if (found) {
+        res.json(customers.filter(customer => customer.email === String(req.params.email)));
+    } else {
+        res.status(400).json({ msg: `No customer with the customer type ${req.params.email}`});
+    }
+});
+
+//getCustomerByBuilding
+app.get("/getCustomerByBuilding/:buildings", (req,res) => {
+    const found = customers.some(customer => customer.buildings.includes(perseInt(req.params.buildings)));
+    if (found) {
+        res.json(customers.filter(customer => customer.buildings.includes(perseInt(req.params.buildings))));
+    } else {
+        res.status(400).json({ msg: `No customer with the customer type ${req.params.buildings}`});
+    }
+});
+
+//getCustomerByFiscalAdress
+app.get("/getCustomerByFiscalAdress/:fiscal_address", (req,res) => {
+    const found = customers.some(customer => customer.fiscal_address === String(req.params.fiscal_address));
+    if (found) {
+        res.json(customers.filter(customer => customer.fiscal_address === String(req.params.fiscal_address)));
+    } else {
+        res.status(400).json({ msg: `No customer with the customer type ${req.params.fiscal_address}`});
+    }
+});
+
+//deleteCustomerById
+app.get("/deleteCustomerById/:id", (req,res) => {
+    const found = customers.some(customer => customer.id === parseInt(req.params.id));
+    if (found) {
+        res.json({ 
+        msg: "Member deleted",
+        boilers: customers.filter(customer => customer.id !== parseInt(req.params.id))
+        });
+    } else {
+        res.status(400).json({ msg: `No customer with the id ${req.params.id}`});
+    }
+});
