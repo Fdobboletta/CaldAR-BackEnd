271764d20d1a74812d08acdc326a4053e39cb0a2
Santiago Villa <santiago.villa@radiumrocket.com>
2020-11-24 13:06:49 -0300

    CALD-007: Syntax fixes

--- controllers/BoilerTypesController.js
+++ controllers/BoilerTypesController.js
@@ -1,68 +1,86 @@
 const express = require('express');
 const app = express();
-const boilerTypes = require ("../data/BoilerTypes.json")
+const boilerTypes = require("../data/BoilerTypes.json")
 const PORT = process.env.PORT || 5000;
 
 // getAllBoilerTypes
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.json(boilerTypes)
 });
 
 // getBoilerTypeById
 app.get("/getBoilerTypeById/:id", (req, res) => {
-    const found = boilerTypes.some(boilerTypes => boilerTypes.id === parseInt(req.params.id));
+    const found = boilerTypes.some(boilerTypes => {
+        boilerTypes.id === parseInt(req.params.id)
+    })
 
     if (found) {
-        res.json(boilerTypes.filter(boilerTypes => boilerTypes.id === parseInt(req.params.id)));
+        res.json(boilerTypes.filter(boilerTypes => {
+            boilerTypes.id === parseInt(req.params.id)
+        }))
     } else {
-        res.status(400).json({ msg: `No boiler type with the id of ${req.params.id}`})
+        res.status(400).json({ msg: `No boiler type with the id of ${req.params.id}` })
     }
 });
 
 // getBoilerTypeBySkillsId
 app.get("/getBoilerTypesBySkillsId/:skillsId", (req, res) => {
     const skillsIdNumber = parseInt(req.params.skillsId);
-    const found = boilerTypes.some(boilerTypes => boilerTypes.skillsId.includes(skillsIdNumber));
+    const found = boilerTypes.some(boilerTypes => {
+        boilerTypes.skillsId.includes(skillsIdNumber)
+    });
 
     if (found) {
-        res.json(boilerTypes.filter(boilerTypes => boilerTypes.skillsId.includes(skillsIdNumber)))
+        res.json(boilerTypes.filter(boilerTypes => {
+            boilerTypes.skillsId.includes(skillsIdNumber)
+        }))
     } else {
-        res.status(400).json({ msg: `No boiler type with the skill id of ${req.params.skillsId}`})
+        res.status(400).json({ msg: `No boiler type with the skill id of ${req.params.skillsId}` })
     }
 });
 
 // getBoilerTypesByDescription
-app.get("/getBoilerTypesByDescription/:description", (req, res) =>{
-    const found = boilerTypes.some(boilerTypes => boilerTypes.description === String(req.params.description));
+app.get("/getBoilerTypesByDescription/:description", (req, res) => {
+    const found = boilerTypes.some(boilerTypes => {
+        boilerTypes.description === String(req.params.description)
+    });
 
     if (found) {
-        res.json(boilerTypes.filter(boilerTypes => boilerTypes.description === String(req.params.description)))
+        res.json(boilerTypes.filter(boilerTypes => {
+            boilerTypes.description === String(req.params.description)
+        }))
     } else {
-        res.status(400).json({ msg: `No boiler type with the description of ${req.params.description}`})
+        res.status(400).json({ msg: `No boiler type with the description of ${req.params.description}` })
     }
 });
 
 // getBoilerTypesByStock
 app.get("/getBoilerTypesByStock/:stock", (req, res) => {
-    const found = boilerTypes.some(boilerTypes => boilerTypes.stock === parseInt(req.params.stock));
+    const found = boilerTypes.some(boilerTypes => {
+        boilerTypes.stock === parseInt(req.params.stock)
+    });
 
     if (found) {
-        res.json(boilerTypes.filter(boilerTypes => boilerTypes.stock === parseInt(req.params.stock)));
+        res.json(boilerTypes.filter(boilerTypes => {
+            boilerTypes.stock === parseInt(req.params.stock)
+        }));
     } else {
-        res.status(400).json({ msg: `No boiler type with the stock of ${req.params.stock}`})
+        res.status(400).json({ msg: `No boiler type with the stock of ${req.params.stock}` })
     }
 });
 
 // deleteBoilerById
-app.delete ("/:id", (req, res) => {
-    const found = boilerTypes.some(boilerTypes => boilerTypes.id === parseInt(req.params.id))
-    
-    if (found){
-        res.json({ 
-            msg: `Member with the id ${req.params.id} deleted`, 
+app.delete("/:id", (req, res) => {
+    const found = boilerTypes.some(boilerTypes => {
+        boilerTypes.id === parseInt(req.params.id)
+    })
+
+    if (found) {
+        res.json({
+            msg: `Member with the id ${req.params.id} deleted`,
         });
     } else {
-        res.status(400).json({ msm: `No member with the id of ${req.params.id}`});
+        res.status(400).json({ msm: `No member with the id of ${req.params.id}` });
     }
 })
 
