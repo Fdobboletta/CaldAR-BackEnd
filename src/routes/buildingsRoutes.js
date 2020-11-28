60faf9eecfde2ad589399826a2d4e00e356811f3
patoiwa <patoiwa@gmail.com>
2020-11-28 20:22:09 -0300

    Mistake correted in routes

--- src/routes/buildingsRoutes.js
+++ src/routes/buildingsRoutes.js
@@ -7,6 +7,10 @@ router.post("/", buildings.create);
 // Get all Buildings
 router.get("/", buildings.findAll);
 
+// Get Buildings by Id
+router.get("/:id", buildings.findById);
+
 // Delete a Building
+router.delete("/:id", buildings.delete);
 
 module.exports = router;
