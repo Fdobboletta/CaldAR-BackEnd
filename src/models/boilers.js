ebc4360652f40a59b0483152a05fa52675c74c91
Martina Moser <mmoser.ar@gmail.com>
2020-11-28 11:46:06 -0300

    Created in folder  models the schema for boilers

---
+++ src/models/boilers.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const boilersSchema = new Schema(
+    {
+        description:{
+            type: String,
+            required: true,
+        },
+        boilerType:{
+            type: String,
+            required: true,
+        },
+        maintenance_period:{
+            type: String,
+            required: true, 
+        },
+        hour_maintenance_cost:{
+            type: Number,
+            required: true,
+        },
+        hour_eventual_cost: {
+            type: Number,
+            required: true,
+        }
+    }
+)
+export default Mongoose.model('boilers', boilersSchema);
