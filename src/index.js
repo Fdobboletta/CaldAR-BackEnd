2715756993ed28aa304fd9dc6508134a8211ef14
Santiago Villa <santiagovillavega@gmail.com>
2020-12-01 09:17:10 -0300

    Body Parser implemented in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const router = require('./routes');
 const mongoose = require('mongoose');
 const PORT = 4000;
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 mongoose.connect('mongodb+srv://CaldAr-m1:caldar@caldar-m1.rpj9l.mongodb.net/CaldAr?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, ).then(()=> {
     console.log('DB Connected')
