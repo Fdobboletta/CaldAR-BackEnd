017a582f7a3e457b71eeeec65108e190a0693bf1
patoiwa <patoiwa@gmail.com>
2020-12-01 09:57:29 -0300

    Missing routes added

--- src/routes/buildingsRoutes.js
+++ src/routes/buildingsRoutes.js
@@ -10,6 +10,12 @@ router.get("/", buildings.findAll);
 // Get Buildings by Id
 router.get("/:id", buildings.findById);
 
+//Get Buildings by attribute
+router.get("/attribute/:companyId", buildings.findByAttribute);
+
+//Update a Building by Id
+router.put("/:id", buildings.update);
+
 // Delete a Building
 router.delete("/:id", buildings.delete);
 
