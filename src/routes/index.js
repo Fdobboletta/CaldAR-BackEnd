5e22d22b6359f1cf0d3670e014cfcaa046c2afbe
patoiwa <patoiwa@gmail.com>
2020-12-03 10:14:59 -0300

    Chacked version

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,15 @@ const boilersRouter = require('./boilersRoutes');
 const technicianRouter = require('./technicianRoutes');
 const appointmentsRouter = require('./appointmentsRoutes');
 const express = require('express');
+
 const router = express.Router();
 
 router.use('/types', boilerTypesRouter);
+router.use('/buildings', buildingsRouter);
 router.use('/company', companyRouter);
 router.use('/boilers', boilersRouter);
 router.use('/technicians', technicianRouter);
 router.use('/appointment', appointmentsRouter);
-router.use('/buildings', buildingsRouter);
+
 
 module.exports = router;
