8262588a66e6d46779f70dc93fa47ac6a156624a
samueltrillo <knubbe10@gmail.com>
2020-12-09 13:51:09 -0300

    Procfile added; env variable added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./routes');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const bodyParser = require('body-parser');
 
